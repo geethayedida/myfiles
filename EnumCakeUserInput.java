4c731ecbe48d2596ecc2f4d085f38cbfa448bf06
Geetha Yedida <yedida.geetha@gmail.com>
2016-02-05 14:12:18 -0700

    new

--- EnumCakeUserInput.java
+++ EnumCakeUserInput.java
@@ -1,22 +1,7 @@
-package com.first.Jan92016;
+package Jan92016.myfiles;
 
 /**
  * Created by Geetha Yedida on 1/22/2016.
  */
-public class EnumCakeUserInput extends ExceptionHandling{
-
-
-    public enum cakeModelAndPrice{
-        BlueCream(25), PinkCream(25),BarbieCake(60),DisneyCake(90),AngelCake(120),FrozenCake(200);
-        private int value;
-
-        private cakeModelAndPrice(int value){
-            this.value = value;
-        }
-    }
-
-    public static void main(String[] args) {
-        EnumCakeUserInput enumcake = new EnumCakeUserInput();
-        enumcake.welcomeMessage();
-    }
+public class EnumCakeUserInput{
 }
